2cd421fdaacbe841336d531c8b501e4f4350c347
bhzy <igor.prudkyi@gmail.com>
2021-03-16 13:39:30 +0200

    Update select.sql
    Formatting

--- select.sql
+++ select.sql
@@ -1,23 +1,39 @@
 USE AdventureWorks2019
 GO
+--Command 1
 SELECT * 
 FROM HumanResources.Department
 WHERE GroupName LIKE '%Research%'
 ORDER BY DepartmentId DESC
 GO
-SELECT BusinessEntityID, JobTitle, BirthDate, Gender, NationalIDNumber 
+--Command 2
+SELECT BusinessEntityID, 
+	JobTitle, 
+	BirthDate, 
+	Gender, 
+	NationalIDNumber 
 FROM HumanResources.Employee
 WHERE NationalIDNumber BETWEEN 500000000 AND 1000000000
 GO
-SELECT BusinessEntityID, JobTitle, BirthDate, Gender
+--Command 3
+SELECT BusinessEntityID, 
+	JobTitle, 
+	BirthDate, 
+	Gender
 FROM HumanResources.Employee
 WHERE YEAR(BirthDate) IN (1980, 1990)
 GO
-SELECT BusinessEntityID, ShiftID
+--Command 4
+SELECT BusinessEntityID, 
+	ShiftID
 FROM HumanResources.EmployeeDepartmentHistory
-GROUP BY BusinessEntityID, ShiftID
+GROUP BY BusinessEntityID, 
+	ShiftID
 GO
-SELECT BusinessEntityID, ShiftID
+--Command 5
+SELECT BusinessEntityID, 
+	ShiftID
 FROM HumanResources.EmployeeDepartmentHistory
-GROUP BY BusinessEntityID, ShiftID
+GROUP BY BusinessEntityID, 
+	ShiftID
 HAVING COUNT(*) >= 2
