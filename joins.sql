1fb28bbc23e753e7772a9692f94d47d340fecebe
bhzy <igor.prudkyi@gmail.com>
2021-03-24 15:54:30 +0200

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -2,115 +2,116 @@ USE AdventureWorks2019;
 GO
 -- Query 1
 SELECT
-	p.FirstName
+    p.FirstName
   , p.LastName
   , e.JobTitle
   , e.BirthDate
 FROM
-	Person.Person AS p
-	INNER JOIN
-		HumanResources.Employee AS e
-		ON
-			p.BusinessEntityID = e.BusinessEntityID
+    Person.Person AS p
+    INNER JOIN
+        HumanResources.Employee AS e
+        ON
+            p.BusinessEntityID = e.BusinessEntityID
 ;
 
 GO
 -- Query 2
 SELECT
-	p.FirstName
+    p.FirstName
   , p.LastName
   , (
-		SELECT
-			JobTitle
-		FROM
-			HumanResources.Employee AS e
-		WHERE
-			e.BusinessEntityID = p.BusinessEntityID
-	)
-	AS JobTitle
+        SELECT
+            JobTitle
+        FROM
+            HumanResources.Employee AS e
+        WHERE
+            e.BusinessEntityID = p.BusinessEntityID
+    )
+    AS JobTitle
 FROM
-	Person.Person AS p
+    Person.Person AS p
 ;
 
 GO
 -- Query 3
-SELECT t.*
+SELECT
+    t.*
 FROM
-	(
-		SELECT
-			p.FirstName
-		  , p.LastName
-		  , (
-				SELECT
-					e.JobTitle
-				FROM
-					HumanResources.Employee AS e
-				WHERE
-					e.BusinessEntityID = p.BusinessEntityID
-			)
-			AS JobTitle
-		FROM
-			Person.Person AS p
-	)
-	AS t
+    (
+        SELECT
+            p.FirstName
+          , p.LastName
+          , (
+                SELECT
+                    e.JobTitle
+                FROM
+                    HumanResources.Employee AS e
+                WHERE
+                    e.BusinessEntityID = p.BusinessEntityID
+            )
+            AS JobTitle
+        FROM
+            Person.Person AS p
+    )
+    AS t
 WHERE
-	t.JobTitle IS NOT NULL
+    t.JobTitle IS NOT NULL
 ;
 
 GO
 -- Query 4
 SELECT
-	p.FirstName
+    p.FirstName
   , p.LastName
   , e.JobTitle
 FROM
-	(
-		SELECT DISTINCT
-			FirstName
-		  , LastName
-		FROM
-			Person.Person
-	)
-	as p
-	CROSS JOIN
-		(
-			SELECT DISTINCT
-				JobTitle
-			FROM
-				HumanResources.Employee
-		)
-		as e
+    (
+        SELECT DISTINCT
+            FirstName
+          , LastName
+        FROM
+            Person.Person
+    )
+    as p
+    CROSS JOIN
+        (
+            SELECT DISTINCT
+                JobTitle
+            FROM
+                HumanResources.Employee
+        )
+        as e
 ;
 
 GO
 -- Query 5
 SELECT
-	COUNT(*)
+    COUNT(*)
 FROM
-	(
-		SELECT
-			p.FirstName
-		  , p.LastName
-		  , e.JobTitle
-		FROM
-			(
-				SELECT DISTINCT
-					FirstName
-				  , LastName
-				FROM
-					Person.Person
-			)
-			as p
-			CROSS JOIN
-				(
-					SELECT DISTINCT
-						JobTitle
-					FROM
-						HumanResources.Employee
-				)
-				as e
-	)
-	AS t
+    (
+        SELECT
+            p.FirstName
+          , p.LastName
+          , e.JobTitle
+        FROM
+            (
+                SELECT DISTINCT
+                    FirstName
+                  , LastName
+                FROM
+                    Person.Person
+            )
+            as p
+            CROSS JOIN
+                (
+                    SELECT DISTINCT
+                        JobTitle
+                    FROM
+                        HumanResources.Employee
+                )
+                as e
+    )
+    AS t
 ;
 
 GO
